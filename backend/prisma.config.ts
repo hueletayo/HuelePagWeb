7babba4c67f85a5389628cebc95f31285a8a49c1
Hueletayo <danielrod400@gmail.com>
2026-08-21 14:57:19 -0400

    fix(backend): fix prisma generation and tsconfig for render build

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from '@prisma/config';
 export default defineConfig({
-  earlyAccess: true,
   datasource: {
     url: process.env.DATABASE_URL || "postgresql://neondb_owner:npg_YXElVT6zweZ9@ep-royal-pine-aybca0dt.c-5.us-east-2.aws.neon.tech/neondb?sslmode=require",
   }
